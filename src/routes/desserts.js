82b23f7140020f97c6fc891667bb3d406926be1c
Danch0 <iamguzmand@gmail.com>
2022-01-07 11:46:50 -0600

    add functions get all desserts and insert

--- src/routes/desserts.js
+++ src/routes/desserts.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
 const router = Router();
 const desserts = require('../sample.json');
+const dessertsDB = require('../sample.json');
+const knex = require('../knex');
 
-router.get('/', (req, res) => {
-    res.json(desserts);
+router.get('/', async (req, res) => {
+    const response = await knex.select('*').from('desserts');
+    res.json(response);
 });
+
+router.post('/new', async (req, res) => {
+    const respose = await knex('desserts').insert({name: req.body.name});
+    res.json(respose)
+})
 
 module.exports = router;
