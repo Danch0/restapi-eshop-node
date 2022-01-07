854217bd000a82e2026d3b9f2a3ac5ac815bc7b6
Danch0 <iamguzmand@gmail.com>
2022-01-07 11:45:40 -0600

    Configuration of knex

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 // setings
 app.set('port', process.env.PORT || 3030);
 app.set('json spaces', 2);
 
-// middlewares
+// middlewar
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use('/api/test', require('./routes/index'));
@@ -17,5 +19,5 @@ app.use('/api/desserts', require('./routes/desserts'));
 
 // stating server
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${3030}`);
+    console.log(`Server on port ${app.get('port')}`);
 });
